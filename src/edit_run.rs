a3c4ed0b21bdb99f8e40ea1e9d5c8198627f897c
Riccardo Mazzarini <me@noib3.dev>
2023-06-08 02:00:15 +0200

    implement `RunId::between`

--- src/edit_run.rs
+++ src/edit_run.rs
@@ -131,7 +131,7 @@ impl EditRun {
     #[inline]
     pub fn split(&mut self, byte_offset: usize) -> Option<Self> {
         if byte_offset < self.len {
-            let mut rest = *self;
+            let mut rest = self.clone();
             rest.len = self.len - byte_offset;
             self.len = byte_offset;
             Some(rest)
@@ -143,21 +143,46 @@ impl EditRun {
 
 /// TODO: docs
 ///
-/// The `Ord` implementation for `Vec`s is a lexicographic sort, so we can
-/// derive those traits.
+/// The `Ord` implementation for `Vec`s [is already][lexi] a lexicographic
+/// sort, so we can just derive those traits.
+///
+/// [lexi]: https://doc.rust-lang.org/std/vec/struct.Vec.html#impl-Ord-for-Vec<,+A>
 #[derive(Debug, Clone, Default, PartialEq, Eq, PartialOrd, Ord)]
 pub struct RunId {
     /// TODO: docs
     letters: Vec<u16>,
 }
 
+impl RunId {
+    /// TODO: docs
+    pub fn between(left: &Self, right: &Self) -> Self {
+        debug_assert!(left < right);
+
+        let mut letters = Vec::new();
+
+        let left_then_zero =
+            left.letters.iter().copied().chain(core::iter::repeat(0));
+
+        let right_then_max =
+            right.letters.iter().copied().chain(core::iter::repeat(u16::MAX));
+
+        for (left, right) in left_then_zero.zip(right_then_max) {
+            let halfway = (left + right) / 2;
+
+            letters.push(halfway);
+
+            if halfway != left {
+                break;
+            }
+        }
+
+        Self { letters }
+    }
+}
+
 impl AddAssign<&Self> for RunId {
     #[inline]
-    fn add_assign(&mut self, other: &Self) {
-        if &*self < other {
-            *self = other.clone();
-        }
-    }
+    fn add_assign(&mut self, other: &Self) {}
 }
 
 impl SubAssign<&Self> for RunId {
@@ -185,39 +210,45 @@ impl core::fmt::Debug for RunSummary {
     }
 }
 
-impl Add<Self> for RunSummary {
+impl AddAssign<&Self> for RunSummary {
+    #[inline]
+    fn add_assign(&mut self, other: &Self) {
+        self.len += other.len;
+
+        if self.max_run_id < other.max_run_id {
+            self.max_run_id = other.max_run_id.clone();
+        }
+    }
+}
+
+impl Add<&Self> for RunSummary {
     type Output = Self;
 
     #[inline]
-    fn add(mut self, rhs: Self) -> Self {
+    fn add(mut self, rhs: &Self) -> Self {
         self += rhs;
         self
     }
 }
 
-impl AddAssign<Self> for RunSummary {
+impl SubAssign<&Self> for RunSummary {
     #[inline]
-    fn add_assign(&mut self, other: Self) {
-        self.len += other.len;
-        self.max_run_id += &other.max_run_id;
+    fn sub_assign(&mut self, other: &Self) {
+        self.len -= other.len;
+
+        if self.max_run_id > other.max_run_id {
+            self.max_run_id = other.max_run_id.clone();
+        }
     }
 }
 
-impl Sub<Self> for RunSummary {
+impl Sub<&Self> for RunSummary {
     type Output = Self;
 
     #[inline]
-    fn sub(mut self, rhs: Self) -> Self {
+    fn sub(mut self, rhs: &Self) -> Self {
         self -= rhs;
         self
-    }
-}
-
-impl SubAssign<Self> for RunSummary {
-    #[inline]
-    fn sub_assign(&mut self, other: Self) {
-        self.len -= other.len;
-        self.max_run_id -= &other.max_run_id;
     }
 }
 
@@ -230,5 +261,23 @@ impl Summarize for EditRun {
             len: self.len * (self.is_visible as usize),
             max_run_id: self.run_id.clone(),
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    impl<I: IntoIterator<Item = u16>> From<I> for RunId {
+        fn from(iter: I) -> Self {
+            Self { letters: iter.into_iter().collect() }
+        }
+    }
+
+    #[test]
+    fn run_id_0() {
+        let left = RunId::from([1]);
+        let right = RunId::from([3]);
+        assert_eq!(RunId::between(&left, &right), RunId::from([2]));
     }
 }
